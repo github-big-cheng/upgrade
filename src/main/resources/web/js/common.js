1d35fa75e960ab777deee1e7de05b5f9f7da5f56
github-big-cheng <329728547@qq.com>
2020-08-29 21:19:41 +0800

    js adjustment

--- src/main/resources/web/js/common.js
+++ src/main/resources/web/js/common.js
@@ -45,6 +45,55 @@ const CommonJS = {
                 return name;
             }
         }).appendTo($("#"+target));
-    }
+    },
+
+    /**
+     * 排序
+     * @param arr 要排序的数组
+     * @param _Comparator 比较器
+     */
+    sort : function(arr, _Comparator){
+        _doSort(arr, 0, arr.length-1, [], _Comparator);
+
+        function _doSort(arr, left, right, temp, _Comparator){
+            if(left < right){
+                let mid = parseInt((left + right) / 2);
+                _doSort(arr, left, mid, temp, _Comparator); // 左子序排序
+                _doSort(arr, mid+1, right, temp, _Comparator); // 右子序排序
+                _merge(arr, left, mid, right, temp, _Comparator); // 合并
+            }
+        };
+        function _merge(arr, left, mid, right, temp, _Comparator){
+            let i = left; // 左序列指针
+            let j = mid + 1; // 右序列指针
+            let k = 0; // 临时下标
+
+            // 比较左右序列大小，将小的放入临时数据中
+            while(i<=mid && j<=right){
+                if(_Comparator(arr[i], arr[j]) > 0){
+                    temp[k++] = arr[j++];
+                }else{
+                    temp[k++] = arr[i++];
+                }
+            }
+
+            // 将左序列剩余元素放入临时数组
+            while(i <= mid){
+                temp[k++] = arr[i++];
+            }
+
+            // 将右序列生于元素放入临时数组
+            while(j <= right){
+                temp[k++] = arr[j++];
+            }
+
+            // 排序后数组复制回原数组
+            k = 0;
+            while(left <= right){
+                arr[left++] = temp[k++];
+            }
+        };
+    },
+
 
 };
