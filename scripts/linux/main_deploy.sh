5ed24547d4e414b9005ea49ff03eafdeb8c97d60
yi.cheng <32728547@qq.com>
2020-09-16 13:55:45 +0800

    about sql deploy

--- scripts/linux/main_deploy.sh
+++ scripts/linux/main_deploy.sh
@@ -2,7 +2,7 @@
 
 # 参数校验
 
-if [ ! -n "$1" ]; then
+if [ -z "$1" ]; then
         echo "please have a parameter of new file name with full path, such as /home/dounion/download/dosmart-master.zip"
         exit 1
 fi
@@ -14,13 +14,13 @@ fi
 echo "待更新的文件：【$1】"
 
 
-if [ ! -n "$2" ]; then
+if [ -z "$2" ]; then
         echo "please have a parameter of work path, such as /home/dounion/server"
         exit 3
 fi
 echo "工作目录路径：【$2】"
 
-if [ ! -n "$3" ]; then
+if [ -z "$3" ]; then
         echo "please have a parameter of old file name, such as dosmart-master"
         exit 4
 fi
@@ -34,22 +34,12 @@ echo "操作的文件名：【$3】"
 
 
 
-# 停应用
-# 1. 首先查询是否有pid文件
-for n in `find "$2" -name "*.pid"`; do
-	PID=$(cat "$n")
-	if [ ! -n "$PID" ]; then
-		kill -9 "$PID"
-		echo "found pid 【$PID】 in file【$n】"
-		sleep 3
-	fi
-done
-# 2. 按应用名称查询进程号
+# 停应用 按应用名称查询进程号
 echo "$progress_name"
 if [ "$PID" = 0 ]; then
 	PID=$(ps -ef | grep "$progress_name" | grep -v grep | grep 'java' | awk '{print $2}')	
-	if [ ! -n "$PID" ]; then
-		for p in "$PID"
+	if [ -z "$PID" ]; then
+		for p in $PID
 		do
 			kill -9 "$p"
                 	echo "killed progress by ps, pid is 【$p】"
---
+++ scripts/linux/sql_deploy.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# 库点代码
+CODE=$1
+echo "$CODE"
+if [ -z "$CODE" ]; then
+	echo "please have a parameter of dept code, such as dounion118"
+	exit 1
+fi
+
+# sql文件压缩包路径
+ZIP_FILE=$2
+echo "$ZIP_FILE"
+if [ -z "$ZIP_FILE" ]; then
+	echo "please have a parameter of sql's zip file with full path, such as /home/dounion/download/upgrade/sql.zip"
+	exit 2
+fi
+if [ ! -f "$ZIP_FILE" ]; then
+	# 检查文件是否存在
+	echo "file doesn't exists, please check it"
+	exit 3
+fi
+
+
+# 获取当前是否存在已解压的文件夹
+WORK_DIR=${ZIP_FILE%/*}
+echo "work dir is $WORK_DIR"
+DIR_NAME=${ZIP_FILE##/*}
+DIR_NAME=${ZIP_FILE%.*}
+echo "dir name is $DIR_NAME"
+cd "$WORK_DIR"/
+if [ -d "$DIR_NAME" ]; then
+	echo "dir [$DIR_NAME]  has exists, do delete operation"
+fi
+
+
+# 解压zip包
+unzip -o "$ZIP_FILE"
+
+
+# 检查是否解压成功
+if [ ! -d "$DIR_NAME" ]; then
+	echo "unzip failed..."
+	exit 4
+fi
+
+
+# loop the sql file in dir
+echo "ls $DIR_NAME"
+targetDir=`ls $DIR_NAME`
+for file in $targetDir
+do
+	echo "file is $file"
+	if [[ ${file:0-4} == '.sql' ]]; then
+	
+		# set parameters as sql variables && add them to the first line of t.sql
+		sed -i "2 i\ set @code='$CODE';" "$DIR_NAME"/"$file"
+	
+		# execute sql script
+		mysql < "$DIR_NAME"/"$file"
+
+		# clear sql script variables
+		sed -i "2d" "$DIR_NAME"/"$file"
+	fi
+done
+
+# 删除解压后的文件夹
+rm -rf "$DIR_NAME"
