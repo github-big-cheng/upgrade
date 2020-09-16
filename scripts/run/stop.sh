910686e262b05d6bc0ed3f4c9d7acb26f167268c
yi.cheng <32728547@qq.com>
2020-09-16 17:36:00 +0800

    stop.sh

--- scripts/run/stop.sh
+++ scripts/run/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 查找进程
-pid=$(ps -ef|grep 'upgrade'|grep -v grep|awk '{print $2}')
+pid=$(ps -ef|grep 'upgrade'|grep java |grep -v grep|awk '{print $2}')
 if [ -n "$pid" ]; then
 	for p in $pid
 	do
