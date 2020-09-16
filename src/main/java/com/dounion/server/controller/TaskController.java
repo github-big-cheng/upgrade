6ea544d57f432055f5179d1f4d99bb69c46c60ac
yi.cheng <32728547@qq.com>
2020-09-16 18:00:26 +0800

    add wake up btn

--- src/main/java/com/dounion/server/controller/TaskController.java
+++ src/main/java/com/dounion/server/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.dounion.server.controller;
 
+import com.dounion.server.core.base.BaseTask;
 import com.dounion.server.core.request.ResponseBuilder;
 import com.dounion.server.core.request.annotation.RequestMapping;
 import com.dounion.server.core.request.annotation.ResponseType;
@@ -15,12 +16,20 @@ import org.springframework.stereotype.Controller;
 public class TaskController {
 
 
+    /**
+     * 列表页
+     * @return
+     */
     @RequestMapping(name = "任务列表", value = "/list")
     public String list(){
         return "task/list.html";
     }
 
 
+    /**
+     * 列表数据
+     * @return
+     */
     @RequestMapping("/list.json")
     @ResponseType(ResponseTypeEnum.JSON)
     public Object listJson(){
@@ -28,9 +37,31 @@ public class TaskController {
     }
 
 
+    /**
+     * 发送中断指令
+     * @param taskId
+     * @return
+     */
     @RequestMapping("/interrupted.json")
     @ResponseType(ResponseTypeEnum.JSON)
     public Object interrupted(int taskId) {
         return ResponseBuilder.buildSuccess(TaskHandler.interrupted(taskId));
+    }
+
+
+    /**
+     * 唤醒
+     * @param taskId
+     * @return
+     */
+    @RequestMapping("/wakeup.json")
+    @ResponseType(ResponseTypeEnum.JSON)
+    public Object wakeup(int taskId) {
+        BaseTask task = TaskHandler.getTask(taskId);
+        boolean flag = TaskHandler.wakeUp(task.getTaskName());
+        if(flag){
+            return ResponseBuilder.buildSuccess();
+        }
+        return ResponseBuilder.buildError();
     }
 }
